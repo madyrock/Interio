f667ea57ed617df59b9502a5c60ff8c410bd776e
Mudassir Ghazi <amaan.khan848@gmail.com>
2025-03-18 04:03:08 +0530

    modified code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,22 +39,22 @@ const Navbar = () => {
             scrolling ? "text-lime-600" : "text-white"
           }`}
         >
-          <Link to="home" smooth={true} duration={500} className="cursor-pointer">
+          <Link to="home" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Home
         </Link>
-        <Link to="portfolio" smooth={true} duration={500} className="cursor-pointer">
+        <Link to="portfolio" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Portfolio
         </Link>
-        <Link to="service" smooth={true} duration={500} className="cursor-pointer">
+        <Link to="service" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Services
         </Link>
-        <Link to="projects" smooth={true} duration={500} className="cursor-pointer">
+        <Link to="projects" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Projects
         </Link>
-        <Link to="work" smooth={true} duration={500} className="cursor-pointer">
+        <Link to="work" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Work
         </Link>
-        <Link to="footer" smooth={true} duration={500} className="cursor-pointer">
+        <Link to="footer" smooth={true} duration={500} className="hover:scale-110  active:text-gray-400 cursor-pointer transition-all duration-300">
           Contact Us
         </Link>
         </ul>
@@ -76,29 +76,39 @@ const Navbar = () => {
           initial={{ y: "-100%" }}
           animate={{ y: isOpen ? "0%" : "-100%" }}
           transition={{ duration: 0.3 }}
-          className="fixed top-0 left-0 w-full bg-black text-white p-5 shadow-lg text-center font-bold"
+          className="fixed top-0 left-0 w-full bg-zinc-900 text-white p-5 shadow-lg text-center font-bold"
         >
           
           <ul className="mt-4 space-y-2">
             <li>
-              <a href="#" className="block hover:scale-110 transition-all p-2 rounded">
+              <Link to="home" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 transition-all p-2 rounded">
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block hover:scale-110 transition-all p-2 rounded">
+              <Link to="portfolio" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 transition-all p-2 rounded">
                 Portfolio
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="block hover:scale-110 transition-all p-2 rounded">
-                Shop
-              </a>
+              <Link to="service" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 transition-all p-2 rounded">
+                Services
+              </Link>
             </li>
             <li>
-              <a href="#" className="block hover:scale-110 transition-all p-2 rounded">
+              <Link to="projects" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 active:text-blue-500 transition-all p-2 rounded">
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link to="work" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 transition-all p-2 rounded">
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link to="footer" smooth={true} duration={500} href="#" onClick={() => setIsOpen(false)} className="block hover:scale-110 transition-all p-2 rounded">
                 Contact Us
-              </a>
+              </Link>
             </li>
           </ul>
         </motion.div>
